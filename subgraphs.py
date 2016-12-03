231a54fadb0d9eeec687255de2f4ecbc5a6381ab
Cole Chamberlin <jessecoleman>
2016-12-02 17:01:39 -0800

    update

--- subgraphs.py
+++ subgraphs.py
@@ -18,5 +18,23 @@ with open('transactions_277198.csv', 'r') as network:
         else:
             curr_tx = []
 graphs = list(nx.connected_component_subgraphs(graph))
+
+users = {}
+back_users = {}
+index = 0
+for g in graphs:
+    users[index] = g.nodes()
+for user in users.keys():
+    for hsh in users[user]:
+        backusers[hsh] = user
+
+with open('transactions_277198.csv', 'r') as network:
+    user_graph = nx.Graph()
+    curr_tx = ''
+    for line in network.readlines():
+        args = line.split(',')
+        new_tx = args[0]
+        if curr_tx[]
+
 #print(graphs[0])
 print(len(graphs))
