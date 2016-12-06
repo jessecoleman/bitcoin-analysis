da9118c83a160024ca945d0135252317edf7e872
colec <colechamberlin@gmail.com>
2016-12-05 23:21:11 -0800

    Almost working!

--- subgraphs.py
+++ subgraphs.py
@@ -33,21 +33,24 @@ def calc_centrality(file_path):
                 pos_count = pos_count + 1
                 #print(str(pos_count) + '/' + str(length))
                 #print(tx_address)
-                graph.add_node(tx_address)
+                if not graph.has_node(tx_address):
+                    graph.add_node(tx_address)
                 #connect all public addresses from the same transaction
                 #print('tx_address: ' + tx_address + ' in_address: ' + in_address)
-                graph.add_edge(tx_address, in_address)
+                if not graph.has_edge(tx_address, in_address):
+                    graph.add_edge(tx_address, in_address)
             else:
                 if tx_flag:
                     tx_count = tx_count + 1
                 tx_flag = False
                 tx_hash = curr_tx_hash
                 in_address = tx_address
-                graph.add_node(in_address)
+                if not graph.has_node(in_address):
+                    graph.add_node(in_address)
                 
 
-    print(tx_count)
-    print(len(graph.nodes()))
+    #print(tx_count)
+    #print(len(graph.nodes()))
     graphs = list(nx.connected_component_subgraphs(graph))
     #graphs = list(ig.components(graph, mode="weak"))
 
@@ -103,10 +106,8 @@ def calc_centrality(file_path):
                     num_not_found = num_not_found + 1
                 if args[1] == ' out':
                     #print('out')
-                    if curr_in_user != curr_user:
+                    if curr_in_user != curr_user and not user_graph.has_edge(curr_in_user, curr_user):
                         user_graph.add_edge(curr_in_user, curr_user, weight=args[5])
-                    if curr_in_user == '19shhoca4GWsTxDbWPad2MyDXbZWwA6c2M':
-                        print('connected')
             else:
                 curr_tx = new_tx
                 in_user_flag = True
@@ -114,51 +115,75 @@ def calc_centrality(file_path):
                     curr_in_user = back_users[args[2].strip()]
                 except:
                     curr_in_user = args[2].strip
-    print('found: ' + str(num_found))
-    print(num_not_found)
+    #print('found: ' + str(num_found))
+    #print(num_not_found)
 
 
     print(nx.info(user_graph))
     in_degree = nx.in_degree_centrality(user_graph)
-    out_degree = nx.out_degree_centrality(user_graph)
+    #out_degree = nx.out_degree_centrality(user_graph)
 
     #pagerank = nx.pagerank_numpy(user_graph, alpha=0.85, weight='weight')
     results1 = pd.DataFrame.from_dict(in_degree, orient='index')
     #results2 = pd.DataFrame.from_dict(out_degree, orient='index')
     #results = pd.merge(results1, results2, left_index=True, right_index=True)
-
-    results = results.sort_values(by='0_x', ascending=False)
-    print(results.head(10))
+    results = results1
+    results = results.sort_values(by=0, ascending=False)
+    #print(results.head(10))
     # return sorted by in degree
-    return (users, results.head(10))
 
+    subset_users = {}
+        # copy subset of users for return
     for i in range(0,10):
-        top_in_user = results.head(10).index[i]
-        print(len(users[top_in_user]))
+        top_in_user = results.head(20).index[i]
+        subset_users[top_in_user] = users[top_in_user]
+    print(len(subset_users))
+    return (subset_users, results.head(20))
 
     results = results.sort_values(by='0_y', ascending=False)
     print(results.head(20))
     top_out_user = results.head(1).index[0]
 
-    for i in range(0,10):
+    for i in range(0, 10):
         top_out_user = results.head(10).index[i]
         print(len(users[top_out_user]))
-    #print(graphs[0])
-    #print(len(graphs))
-    
+
+
 
 #file_path = 'transactions_277198.csv'
 paths = glob.glob('*.csv')
 
-prev_users = ''
+prev_users = {}
 prev_results = ''
+user_traj = {}
+dates = open('dates_blocks.txt', 'r')
 for path in paths:
+    date = dates.readline().split(',')[0]
     users, results = calc_centrality(path)
-    if prev_users != '':
-        for i in range(0, 10):
-            print(results[i])
-            for i in range(0, 10):
-                pass
+    #print(len(users))
+    print(results)
+    if len(prev_users) > 0:
+        for i in users.keys():
+            #print(results[i])
+            for j in prev_users.keys():
+                # check if graphs share any nodes with each other
+                if len(set(users[i]).intersection(set(prev_users[j]))) >= 1:
+                    print(str(i) + ' = ' + str(j))
+                    print(len(set(users[i]).intersection(set(prev_users[j]))))
+                    # update dictionary elements
+                    old_scores = user_traj.get(j, False)
+                    if old_scores:
+                        del user_traj[j]
+                        old_scores.append((date, results.get_value(i, 0)))
+                        user_traj[i] = old_scores
+                    break
+    else:
+        for row in results.itertuples():
+            user_traj[row[0]] = [(date, row[1])]
+    prev_users = users
+    prev_results = results
+    print(user_traj)
 
-
+dates.close()
+print(user_traj)
 
